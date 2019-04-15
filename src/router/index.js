57f7a220af15b204c92a7afb85ff100440ce0569
backthousand <j126x126@126.com>
2019-04-15 21:55:52 +0800

    规划设计列表菜单

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-// in development-env not use lazy-loading, because lazy-loading too many pages will cause webpack hot update too slow. so only in production use lazy-loading;
-// detail: https://panjiachen.github.io/vue-element-admin-site/#/lazy-loading
-
 Vue.use(Router)
 
 /* Layout */
 import Layout from '../views/layout/Layout'
 
 /**
-* hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
-* alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
-*                                if not set alwaysShow, only more than one route under the children
-*                                it will becomes nested mode, otherwise not show the root menu
-* redirect: noredirect           if `redirect:noredirect` will no redirect in the breadcrumb
-* name:'router-name'             the name is used by <keep-alive> (must set!!!)
-* meta : {
-    title: 'title'               the name show in subMenu and breadcrumb (recommend set)
-    icon: 'svg-name'             the icon show in the sidebar
-    breadcrumb: false            if false, the item will hidden in breadcrumb(default is true)
-  }
+ * 本系统主要完成一下四个功能，并依此来列出了菜单
+日常教室表现考核
+日常宿舍表现考核
+奖惩信息统计
+周期性综合考核
 **/
 export const constantRouterMap = [
   { path: '/login', component: () => import('@/views/login/index'), hidden: true },
@@ -29,57 +20,44 @@ export const constantRouterMap = [
   {
     path: '/',
     component: Layout,
-    redirect: '/dashboard',
-    name: 'Dashboard',
+    redirect: '/MoralSystem',
+    name: 'MoralSystem',
     hidden: true,
     children: [{
-      path: 'dashboard',
-      component: () => import('@/views/dashboard/index')
+      path: 'MoralSystem',
+      component: () => import('@/views/MoralSystem/index')
     }]
   },
 
   {
-    path: '/example',
+    path: '/DailyStatus',
     component: Layout,
-    redirect: '/example/table',
+    redirect: '/DailyStatus/classroom',
     name: 'Example',
-    meta: { title: 'Example', icon: 'example' },
+    meta: { title: '日常考核', icon: 'example' },
     children: [
       {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
+        path: 'classroom',
+        name: 'Classroom',
+        component: () => import('@/views/classroom/index'),
+        meta: { title: '日常教室表现考核', icon: 'table' }
       },
       {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
+        path: 'dormitory',
+        name: 'Dormitory',
+        component: () => import('@/views/dormitory/index'),
+        meta: { title: '日常宿舍表现考核', icon: 'table' }
       }
     ]
   },
 
   {
-    path: '/form',
+    path: '/period',
     component: Layout,
-    children: [
-      {
-        path: 'index',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
-      }
-    ]
-  },
-
-  {
-    path: '/nested',
-    component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
+    redirect: '/period/menu1',
+    name: 'Period',
     meta: {
-      title: 'Nested',
+      title: '周期性考核',
       icon: 'nested'
     },
     children: [
@@ -132,12 +110,25 @@ export const constantRouterMap = [
   },
 
   {
-    path: 'external-link',
+    path: '/reward',
     component: Layout,
     children: [
       {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' }
+        path: 'reward',
+        name: 'Reward',
+        component: () => import('@/views/reward/index'),
+        meta: { title: '奖惩信息统计', icon: 'nested' }
+      }
+    ]
+  },
+
+  {
+    path: 'contact',
+    component: Layout,
+    children: [
+      {
+        path: 'http://localhost:9528/#/MoralSystem',
+        meta: { title: '联系我们', icon: 'link' }
       }
     ]
   },
--- src/views/MoralSystem/index.vue
+++ src/views/MoralSystem/index.vue
@@ -9,7 +9,7 @@
 import { mapGetters } from 'vuex'
 
 export default {
-  name: 'Dashboard',
+  name: 'MoralSystem',
   computed: {
     ...mapGetters([
       'name',
